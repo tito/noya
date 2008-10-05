d81e36fbb3d438a31c1603bc6070c96e59c90267
Mathieu Virbel <tito@bankiz.org>
2008-10-05 11:21:29 +0200

    move useful tools from docs to tools. add launcher

---
+++ launch.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+TOOLS_DIR=tools
+REACTIVISION_ZIP=reacTIVision-1.4pre2-src.zip
+REACTIVISION_DIR=reacTIVision-1.4pre2-src
+SIMULATOR_ZIP=TUIO_Simulator-1.3.zip
+SIMULATOR_DIR=TUIO_Simulator
+LIBDC=libdc1394-2.0.2.tar.bz2
+
+function usage()
+{
+	echo "Usage: launch.sh <simulator|reactivision>"
+	exit
+}
+
+function reactivision()
+{
+	pushd . > /dev/null
+
+	cd $TOOLS_DIR/reactivision/
+	if [ ! -d $REACTIVISION_DIR ]; then
+
+		# install libdc
+		tar xjf $LIBDC
+		cd `basename $LIBDC .tar.bz2`
+		./configure && make && sudo make install
+		cd ..
+
+		# install reactivision
+		unzip $REACTIVISION_ZIP
+		cd $REACTIVISION_DIR/linux
+		make
+		cd ../..
+	fi
+
+	cd $REACTIVISION_DIR/linux
+	./reacTIVision
+
+	popd >/dev/null
+}
+
+function simulator()
+{
+	pushd . > /dev/null
+
+	cd $TOOLS_DIR/tuio/
+	if [ ! -d $SIMULATOR_DIR ]; then
+		unzip $SIMULATOR_ZIP
+	fi
+
+	cd TUIO_Simulator
+	java -jar TuioSimulator.jar
+
+	popd >/dev/null
+}
+
+case "$1" in
+	"simulator")
+		simulator;;
+	"reactivision")
+		reactivision;;
+	*)
+		usage;;
+esac
